01006dca3ff0fb9b2ee6e354ffa87b57a3b7bc37
Heriton Lericio <heritonlericio@outlook.com>
2025-10-28 21:15:58 -0300

    Update aula01.js

--- aula01.js
+++ aula01.js
@@ -1,3 +1,8 @@
 //console.log(Quando estq com )
 
 console.log("Hello, World!");
+console.log("Estou aprendendo JavaScript!");
+console.log("JavaScript é uma linguagem de programação divertida.");
+console.log("Vamos criar algo incrível juntos!");
+console.log("Estou animado para aprender mais sobre desenvolvimento web!");
+console.log("Cada linha de código é uma nova aventura.");
